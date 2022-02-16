485617cd4f313fb00cce400bbc5cb074fd7c04a1
pedroj4567 <pedroj4567@gmail.com>
2022-02-16 16:15:30 -0400

    Agregando funcion de eliminar

--- app01.js
+++ app01.js
@@ -1,31 +1,47 @@
-//Variables 
-const nombre = document.getElementById('nombre');
-const apellido = document.getElementById('apellido');
-const cedula = document.getElementById('cedula');
-const correo = document.getElementById('correo');
-const btnAgregar = document.getElementById('btn-agregar');
-const contenedorDatos = document.getElementById('contenedor-datos');
+//Variables
+const nombre = document.getElementById("nombre");
+const apellido = document.getElementById("apellido");
+const cedula = document.getElementById("cedula");
+const correo = document.getElementById("correo");
+const btnAgregar = document.getElementById("btn-agregar");
+const contenedorDatos = document.getElementById("contenedor-datos");
+const btnEliminar = document.querySelector('.eliminar');
+const form = document.querySelector('.formulario');
 let almacenDatos = [];
 let id = 0;
 
-//eventos 
-
-btnAgregar.addEventListener('click', agregarDatos)
+//eventos
+btnAgregar.addEventListener("click", agregarDatos);
+contenedorDatos.addEventListener('click', eliminarDato);
+nombre.addEventListener('blur', validarDato);
+apellido.addEventListener('blur', validarDato);
+cedula.addEventListener('blur', validarDato);
+correo.addEventListener('blur', validarDato);
 //Funciones
+
+function eliminarDato(e){
+    if(e.target.classList.contains('eliminar')){
+      const datoId = e.target.getAttribute('data-id');
+      almacenDatos = almacenDatos.filter((dato) => dato.id != datoId );
+      console.log(almacenDatos)
+      cargarHtml();
+    }
+
+
+}
+
 function agregarDatos(e) {
   e.preventDefault();
-
 
   const datos = {
     nombre: nombre.value,
     apellido: apellido.value,
     cedula: cedula.value,
     correo: correo.value,
-    id : ++id
+    id: ++id,
   };
 
-  almacenDatos = [...almacenDatos,datos]
-
+  almacenDatos = [...almacenDatos, datos];
   console.log(almacenDatos);
 
   //asigna el html
@@ -34,30 +50,31 @@ function agregarDatos(e) {
 }
 
 function cargarHtml(dato) {
-    LimpiarHtml();
-    almacenDatos.forEach((dato)=>{
-       const {nombre,apellido,cedula,correo, id} = dato;
-        const fila = document.createElement('tr');
-        fila.innerHTML = `
+  LimpiarHtml();
+
+  almacenDatos.forEach((dato) => {
+    const { nombre, apellido, cedula, correo, id } = dato;
+    const fila = document.createElement("tr");
+    fila.innerHTML = `
             <td>${id}</td>
             <td>${nombre}</td>
             <td>${apellido}</td>
             <td>${cedula}</td>
             <td>${correo}</td>
-        `
+            <td><p data-id='${id}' class='eliminar'>X</p></td>
+        `;
 
-        contenedorDatos.appendChild(fila);
-    })
-}
+    contenedorDatos.appendChild(fila);
+  });
 
-
-
-function LimpiarHtml(){
-     while(contenedorDatos.firstChild){
-       contenedorDatos.removeChild(contenedorDatos.firstChild);
-     }
-}
-
-function ValidarEntradas(){
+  
   
 }
+
+function LimpiarHtml() {
+  while (contenedorDatos.firstChild) {
+    contenedorDatos.removeChild(contenedorDatos.firstChild);
+  }
+}
+
+
